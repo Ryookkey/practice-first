b753de4e83d41e4ae337ccb369ac82d377f69489
Ryookkey <0818ryoutrou@gmail.com>
2025-04-07 01:22:42 +0900

    create list-controller action new

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  get 'top' => 'homes#top'
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'lists/new'
+  post 'lists' => 'lists#create'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  get '/top' => 'homes#top'
 end
